f881927d6c533e425c470cacb9893caeddc88f88
Opeoluwa  Temitayo Eweje <ewejeopeoluwa@gmail.com>
2016-05-04 16:33:01 -0400

    Sudoku Board Game
    Please find my code files for the implementation of the Sudoku board
    game. The pdf file contains information about the project.
    Please note that the main method is in the Test.java class

---
+++ Test.java
@@ -0,0 +1,22 @@
+
+/**
+ * The class <code>Test</code> is a simple sample test.
+ * 
+ * @author Lucia Moura
+ *
+ */
+public class Test {
+	
+	// this is a sample test; testing WILL NOT be restricted to this case.
+
+	public static void main(String[] args){
+			
+			StudentInfo.display();
+			SudokuGame g1 = new SudokuGame(new SomeSudokuMatrix(1));
+			
+			// for testing other puzzles change the number 1 to 2, 3 or 4 or create your own puzzle via SudokuMatrix interface
+		}
+
+
+	
+}
